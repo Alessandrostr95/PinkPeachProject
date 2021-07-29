f4ad4cef43dfdb8d834bc57a901ea24f4a836ea0
alessandro <alessandro@pop-os.localdomain>
2021-07-30 00:14:40 +0200

    Inserita generazione automatica della home page con gli orari (cartella templates/home/).
    TODO:
    Implementare la creazione della tabella degli orari della magistrale

--- src/site/home/script.js
+++ src/site/home/script.js
@@ -1,11 +1,22 @@
+const triennale = "triennale";
+const magistrale = "magistrale"
+
+var currentState = triennale;
+
 window.onload = function() {
     const slideBar = document.querySelector("#slide-bar");
     const triennale = document.querySelector("#triennale");
-    const magistrale = document.querySelector("#magistrale")
+    const magistrale = document.querySelector("#magistrale");
+    
+    const calendarioTriennale = document.getElementById("calendario-triennale");
+    const calendarioMagistrale = document.getElementById("calendario-magistrale");
 
     document.querySelector("#switch-button").addEventListener("click", function(){
         slideBar.classList.toggle("right");
         triennale.classList.toggle("toggled");
         magistrale.classList.toggle("toggled");
+        currentState = currentState === triennale ? magistrale : triennale;
+        calendarioTriennale.classList.toggle("hidden");
+        calendarioMagistrale.classList.toggle("hidden");
     });
 };
---
+++ src/site/templates/home/generate_homepage.py
@@ -0,0 +1,55 @@
+from jinja2 import Environment, FileSystemLoader
+import csv
+from pprint import pprint
+
+def import_csv(f_name, degree="Triennale"):
+    """
+        Funzione che legge il csv degli orari ottenuto con lo script di Leonardo
+        e ritorna un dizionario con una struttura piu' JSON oriented
+    """
+
+    years = [1, 2, 3] if degree == "Triennale" else [1, 2]
+
+    f = open( f_name )
+    sem = csv.DictReader(f)
+    lines = [line for line in sem]
+
+    data = {}
+    for Y in years:
+        data[Y] = {
+            'lunedì': {},
+            'martedì': {},
+            'mercoledì': {},
+            'giovedì': {},
+            'venerdì': {}
+        }
+    
+    days = ['lunedì', 'martedì', 'mercoledì', 'giovedì', 'venerdì']
+    
+    for line in lines:
+        anno = int(line["anno"])
+        ora = int(line["ora"].split(":")[0])
+        for d in days:
+            materia = line[d].strip().replace("(","<br />(")
+            data[anno][d][ora] = materia if materia != "X" else ""
+    
+    return data
+
+
+if __name__ == "__main__":
+
+    csv_file = "./sem2.csv"
+    result_file = "../../home/index.html"
+
+    dati = import_csv( csv_file )
+    pprint( dati )
+
+    env = Environment( loader=FileSystemLoader(".") )
+    template = env.get_template( './table.html' )
+    output_from_parsed_template = template.render( orari=dati )
+    
+    print( output_from_parsed_template )
+
+    # to save the results
+    with open(result_file, "w") as fh:
+        fh.write( output_from_parsed_template )
