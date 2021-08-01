168ae127492b11cadb5ba2d9cddd7c92e321d2c9
Alessandrostr95 <alessandrostr95@gmail.com>
2021-08-01 13:14:01 +0200

    Aggiunto asset script.js

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,19 @@ const magistrale = "magistrale"
 
 var currentState = triennale;
 
-var coll;
-
 window.onload = function() {
     const slideBar = document.querySelector("#slide-bar");
     const triennale = document.querySelector("#triennale");
     const magistrale = document.querySelector("#magistrale");
     
-    const calendarioTriennale = document.getElementById("calendario-triennale");
-    const calendarioMagistrale = document.getElementById("calendario-magistrale");
+    //const calendarioTriennale = document.getElementById("calendario-triennale");
+    //const calendarioMagistrale = document.getElementById("calendario-magistrale");
+
+    const navTreeTriennale = document.getElementById("nav-tree-triennale");
+    const navTreeMagistrale = document.getElementById("nav-tree-magistrale");
+
+    const newsTriennale = document.getElementById("news-triennale");
+    const newsMagistrale = document.getElementById("news-magistrale");
 
     document.querySelector("#switch-button").addEventListener("click", function(){
         /**
@@ -22,8 +26,12 @@ window.onload = function() {
         triennale.classList.toggle("toggled");
         magistrale.classList.toggle("toggled");
         currentState = currentState === triennale ? magistrale : triennale;
-        calendarioTriennale.classList.toggle("hidden");
-        calendarioMagistrale.classList.toggle("hidden");
+        //calendarioTriennale.classList.toggle("hidden");
+        //calendarioMagistrale.classList.toggle("hidden");
+        navTreeTriennale.classList.toggle("hidden");
+        navTreeMagistrale.classList.toggle("hidden");
+        newsTriennale.classList.toggle("hidden");
+        newsMagistrale.classList.toggle("hidden");
     });
 
 
@@ -50,7 +58,7 @@ window.onload = function() {
 
 
     /* Faccio collassare gli elementi del NAV-TREE quando clicco su un elemento padre */
-    coll = document.querySelectorAll(".tree .tree-node");
+    var coll = document.querySelectorAll(".tree .tree-node");
     console.log( coll );
 
     for(let i = 0; i < coll.length; i++){
@@ -60,8 +68,6 @@ window.onload = function() {
                 // console.log( u );
                 u.classList.toggle("hidden");
             });
-        } else {
-            console.log("asdf");
         }
     }
 
