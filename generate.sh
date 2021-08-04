0cacc17569f9742ff9f6e8eac578d15cdecda1a4
Alessandrostr95 <alessandrostr95@gmail.com>
2021-08-04 19:13:43 +0200

    Modificto script generazione del sito.

--- generate.sh
+++ generate.sh
@@ -15,15 +15,17 @@ main() {
     # -- create new version    
     mkdir $SITE_ROOT
     mkdir $SITE_ROOT/home
-    mkdir $SITE_ROOT/home/triennale
-    mkdir $SITE_ROOT/home/triennale/20-21
-    mkdir $SITE_ROOT/home/magistrale
-    mkdir $SITE_ROOT/home/magistrale/20-21
+    mkdir $SITE_ROOT/triennale
+    mkdir $SITE_ROOT/triennale/20-21
+    mkdir $SITE_ROOT/magistrale
+    mkdir $SITE_ROOT/magistrale/20-21
+
+    cp -r $ASSETS_ROOT $SITE_ROOT
     
     python3 src/site_generator/home.py
     python3 src/site_generator/orari.py
     python3 src/site_generator/docenti.py
-    python3 src/site_generator/assets.py
+    # python3 src/site_generator/assets.py
 }
 
 # call main with all arguments passed from command line
