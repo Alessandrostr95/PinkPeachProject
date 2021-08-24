c12349b0d87e0c3bb2f96a069f06e5bdfc503908
LeonardoE95 <leonardotamiano95@gmail.com>
2021-08-24 17:11:01 +0200

    Tolto file scraper.py e sistemato script generazione generate.sh

--- generate.sh
+++ generate.sh
@@ -9,25 +9,17 @@ export TEMPLATES_ROOT="./templates/"
 export ASSETS_ROOT="./assets/"
 
 main() {
-    # -- clean previous version
-    rm -r $SITE_ROOT
+    # -- clean previous version (?)
+    # rm -r $SITE_ROOT
 
     # -- create new version    
-    mkdir $SITE_ROOT
-    mkdir $SITE_ROOT/home
-    mkdir $SITE_ROOT/triennale
-    mkdir $SITE_ROOT/triennale/20-21
-    mkdir $SITE_ROOT/magistrale
-    mkdir $SITE_ROOT/magistrale/20-21
-
-    # cp -r $ASSETS_ROOT $SITE_ROOT
-    
     python3 src/site_generator/home.py
     python3 src/site_generator/contributors.py
     python3 src/site_generator/orari.py
     python3 src/site_generator/docenti.py
     python3 src/site_generator/corsi.py
-    # python3 src/site_generator/assets.py
+    python3 src/site_generator/lista-corsi.py
+    python3 src/site_generator/esami.py
 }
 
 # call main with all arguments passed from command line
